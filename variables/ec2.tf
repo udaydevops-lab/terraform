1df9dbfad89bb908b9b107c6ca09ab0416f537ea
udaydevops-lab <darauday0@gmail.com>
2024-08-26 16:39:39 +0530

    terraform practice

--- variables/ec2.tf
+++ variables/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web" {
 
-    ami = var.ami
+    ami = var.ami_id
     security_groups = [aws_security_group.allow_all.id]
     instance_type = var.instance_type
     tags = var.tags
